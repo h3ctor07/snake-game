c3875033cd1bb404a1b346cecd4370b0caeeddce
h3ctor07 <hector.penaloza07@gmail.com>
2021-12-24 00:27:53 -0600

    expansion rate reset

--- food.js
+++ food.js
@@ -1,7 +1,7 @@
 import { onSnake, expandSnake } from "./snake.js";
 import {randomGridPosition} from "./grid.js";
 let food = randomGridPosition();
-const EXPANSION_RATE =2;
+const EXPANSION_RATE =1;
 
 export function update(){
     if(onSnake(food)){
